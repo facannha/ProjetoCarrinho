1fba409a014034620467e97f957791f80c5db13b
Guxta <gugmoises@gmail.com>
2022-07-20 15:16:47 -0300

    up

--- store.js
+++ store.js
@@ -26,15 +26,6 @@ function ready() {
     document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 }
 
-function purchaseClicked() {
-    alert('Direcionando para a tela de check-in...')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while (cartItems.hasChildNodes()) {
-        cartItems.removeChild(cartItems.firstChild)
-    }
-    updateCartTotal()
-}
-
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
